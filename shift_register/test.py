c9c9766343e65f1b51a95a9e09b4f054671d3c58
reed-foster <reed.a.foster@gmail.com>
2022-03-09 11:31:39 -0500

    remove testfile

--- shift_register/test.py
+++
@@ -1,7 +0,0 @@
-import phidl.geometry as pg
-from phidl import quickplot as qp
-from phidl import set_quickplot_options
-set_quickplot_options(blocking=True)
-
-D = pg.compass(size = (4,2), layer = 0)
-qp(D) # quickplot the geometry
